28ddc3aef3b8ca8188e8130da54568f9062eee18
Adwait Sharma <adwait.sharma.windows10@gmail.com>
2024-10-09 09:36:26 +0100

    Add files via upload

---
+++ 308c - 2.py
@@ -0,0 +1,9 @@
+#308c - 2
+totalfood = 0
+
+for loop in range(5):
+    food = int(input("Enter the food amt(g): "))
+    totalfood = totalfood + food
+avgfood = totalfood * 0.2
+print (avgfood, "g")
+
---
+++ 308c-1.py
@@ -0,0 +1,8 @@
+#308c - 1
+totaltemp = 0
+
+for loop in range(7):
+    temp = int(input("Enter the temperature:"))
+    totaltemp = totaltemp + temp
+avgtemp = totaltemp * 0.14285714285714285
+print (avgtemp)
